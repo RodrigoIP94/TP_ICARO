fa2c27c9e9a3c8a34b678a4383680e2417bbb42c
RodrigoIP94 <116602947+RodrigoIP94@users.noreply.github.com>
2023-04-23 21:38:59 -0300

    Delete 00_TPGit.js

--- 00_TPGit.js
+++
@@ -1,9 +0,0 @@
-let num1 = 100
-let num2 = 25
-
-function suma (num1, num2){
- let resultado = num1 + num2 ;
-    return(resultado);
-}
-
-console.log("el resultado de num1 + num2 es: ", resultado);
